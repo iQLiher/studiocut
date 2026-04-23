c3f6c12bc9afe96331b0576aa3a40c9aff3311c6
Liher <liher.def@gmail.com>
2026-04-23 12:27:28 +0200

    Delete bg-remover-99 directory

--- bg-remover-99/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title StudioCut - Servidor
-echo Instalando dependencias...
-pip install -r requirements.txt
-echo.
-echo Iniciando servidor en http://localhost:5000
-start http://localhost:5000
-python app.py
-pause
